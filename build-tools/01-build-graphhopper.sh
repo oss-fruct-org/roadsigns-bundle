101c66714d961cebbf5d2ac97bde30b932cf4203
Kirill Ivashov <ivashov@cs.petrsu.ru>
2015-04-14 15:55:16 +0300

    Use https path instead of ssh

--- build-tools/01-build-graphhopper.sh
+++ build-tools/01-build-graphhopper.sh
@@ -3,7 +3,7 @@
 mkdir -p build
 cd build
 
-git clone --depth=1 -b 0.4 git@github.com:graphhopper/graphhopper.git
+git clone --depth=1 -b 0.4 https://github.com/graphhopper/graphhopper.git
 cd graphhopper
 ./graphhopper.sh build
 cp ./core/target/graphhopper-0.4.1.jar $1/lib
