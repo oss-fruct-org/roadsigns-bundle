3d067724170cf6d09fe225e32c34808f2ec0f021
Kirill Ivashov <ivashov@cs.petrsu.ru>
2015-04-14 18:37:04 +0300

    add --complex-ways for osmconvert

--- cut-pbf-fast.sh
+++ cut-pbf-fast.sh
@@ -24,5 +24,5 @@ while read line; do
    FILE_POLY=`echo "$line" | cut -f1`  
    FILE_OUTPUT=`echo "$line" | cut -f2` 
 
-   osmconvert/osmconvert ${FILE_PBF} -B="${DIR_POLY}/${FILE_POLY}" --complete-ways -o="${DIR_OUTPUT}/${FILE_OUTPUT}.osm.pbf"
+   osmconvert/osmconvert ${FILE_PBF} -B="${DIR_POLY}/${FILE_POLY}" --complete-ways --complex-ways -o="${DIR_OUTPUT}/${FILE_OUTPUT}.osm.pbf"
 done < ${FILE_REGIONS}
