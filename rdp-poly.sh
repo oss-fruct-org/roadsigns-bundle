bea834607d5905852fbf86e135f65569a409b022
Kirill Ivashov <ivashov@cs.petrsu.ru>
2015-04-17 16:30:35 +0300

    Handle null regions

--- rdp-poly.sh
+++ rdp-poly.sh
@@ -8,8 +8,10 @@ fi
 INPUT_DIR="$1"
 EPSILON="$2"
 
+shopt -s nullglob
 for f in $INPUT_DIR/*.poly
 do
     ./poly_ramer_douglas_peucker.py "$f" $EPSILON > /tmp/out.poly
     mv /tmp/out.poly $f
 done
+shopt -u nullglob
