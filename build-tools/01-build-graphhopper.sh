4139d0d3b86b6ef91d7697b3fdd03dba83e2428d
Kirill Kulakov <kulakov@cs.karelia.ru>
2016-08-17 15:16:53 +0300

    make graphhopper with dependencies

--- build-tools/01-build-graphhopper.sh
+++ build-tools/01-build-graphhopper.sh
@@ -7,6 +7,7 @@ cd build
 
 git clone --depth=1 -b 0.7.0 https://github.com/graphhopper/graphhopper.git
 cd graphhopper
+git apply <../../graphhopper.patch
 ./graphhopper.sh build
-cp ./core/target/graphhopper-0.7.0.jar $OUT/lib
-ln -s graphhopper-0.7.0.jar $OUT/lib/graphhopper.jar
+cp ./core/target/graphhopper-0.7.0-jar-with-dependencies.jar $OUT/lib
+ln -s graphhopper-0.7.0-jar-with-dependencies.jar $OUT/lib/graphhopper.jar
